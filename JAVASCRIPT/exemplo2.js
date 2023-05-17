47ba844cab7267ee50fdf9b5729983a24e1eb519
Tidusk <gleidisonifro@gmail.com>
2023-05-16 20:37:04 -0400

    JAVASCRIPT

---
+++ JAVASCRIPT/exemplo2.js
@@ -0,0 +1,14 @@
+var n1 = prompt('Informe o primeiro valor');
+var n2 = prompt('Informe o segundo valor');
+
+// parseFloat() para convertar para números reais.
+
+//alert(n1+n2);
+
+result.innerHTML=n1+n2
+
+var resp = confirm('Podemos continuar ?')
+
+document.write("<p> CAVALO"+resp+"</p")
+
+//console.log(n1+n2);
