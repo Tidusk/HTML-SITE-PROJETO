db9bed92f52060426db1c23b45243c74b827952f
Tidusk <gleidisonifro@gmail.com>
2023-05-16 22:04:19 -0400

    sadsad

---
+++ JAVASCRIPT/EXERCICIOS/exercicio1.js
@@ -0,0 +1,5 @@
+var n1 =parseInt=(prompt('Informe o 1° valor: '));
+var n2 =parseInt=(prompt('Informe o 2° valor: '));
+var n3 =parseInt=(prompt('Informe o 3° valor: '));
+var n4 =parseInt=(prompt('Informe o 4° valor: '));
+alert((n1+n2+n3+n4)/4);
